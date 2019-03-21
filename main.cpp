6ff893923ef8375fb34e9d61e3960ea84439b11c
Gabriele Luz <gabrieleluz@infojr.com.br>
2019-03-21 14:41:24 -0300

    adds loop to switch

--- main.cpp
+++ main.cpp
@@ -3,27 +3,26 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
-	char command;
-	
-	scanf("%c", &command);
+int main(int argc, char const *argv[]) {
+  char command;
 
-  	switch(command) {
-  		case 'i':
-  			insert();
-  			break;
-  		case 'c':
-  			consult();
-  			break;
-  		case 'r':
-  			remove();
-  			break;
-  		case 'p':
-  			printAll();
-  			break;
-  		default:
-  			return 0;
-  	}
-
+  do {
+    scanf("%c", &command);
+    switch(command) {
+      case 'i':
+        insert();
+        break;
+      case 'c':
+        consult();
+        break;
+      case 'r':
+        remove();
+        break;
+      case 'p':
+        printAll();
+        break;
+      default:
+        return 0;
+    }
+  } while(scanf("%c", &command));
 }
