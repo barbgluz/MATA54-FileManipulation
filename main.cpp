5c3abc82ee103b344b70c5275b20a0c42eb38a32
Mateus <mateusbarbosa@infojr.com.br>
2019-03-21 22:33:22 -0300

    remove declaração de namespace std

--- main.cpp
+++ main.cpp
@@ -1,28 +1,27 @@
 #include <stdio.h>
 #include "file.h"
 
-using namespace std;
-
 int main(int argc, char const *argv[]) {
-  char command;
+    char command;
 
-  do {
-    scanf("%c", &command);
-    switch(command) {
-      case 'i':
-        insert();
-        break;
-      case 'c':
-        consult();
-        break;
-      case 'r':
-        remove();
-        break;
-      case 'p':
-        printAll();
-        break;
-      default:
-        return 0;
-    }
-  } while(scanf("%c", &command));
+    do {
+        scanf("%c", &command);
+        switch(command) {
+            case 'i':
+                insert();
+                break;
+            case 'c':
+                consult();
+                break;
+            case 'r':
+                remove();
+                break;
+            case 'p':
+                printAll();
+                break;
+            default:
+                return 0;
+        }
+    } while(scanf("%c", &command));
+
 }
